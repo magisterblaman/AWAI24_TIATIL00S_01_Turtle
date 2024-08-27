8a84e8bae5f15f21660bca0af8187e58f074aacb
hannesblaman <hannes@blaman.com>
2024-08-27 10:16:39 +0200

    Program 1, 2 och påbörjat flaggan

---
+++ main.py
@@ -0,0 +1,42 @@
+from turtle import *
+
+speed(1)
+
+# Program 1
+
+# forward(200)
+
+# Program 2
+
+# forward(200)
+# right(90)
+# forward(100)
+# right(90)
+# forward(200)
+# right(90)
+# forward(100)
+
+# Program 3
+
+# blå bakgrund
+fillcolor("blue")
+
+begin_fill()
+forward(300)
+right(90)
+forward(150)
+right(90)
+forward(300)
+right(90)
+forward(150)
+end_fill()
+
+right(90)
+penup()
+forward(60)
+pendown()
+
+fillcolor("yellow")
+
+
+done()
